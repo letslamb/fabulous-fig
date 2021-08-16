564d53fd9421acbbf4574b8f07b421adbf092256
Michael Lamb <mflambjr@gmail.com>
2021-08-16 17:07:10 -0400

    add & configure mdsvex to process markdown files

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,26 @@
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
 import adapter from '@sveltejs/adapter-vercel'
 import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-    adapter: adapter(),
-    vite: {
-      resolve: {
-        alias: {
-          $static: path.resolve('static')
-        }
+  "extensions": [".svelte", ...mdsvexConfig.extensions],
+
+  kit: {
+      // hydrate the <div id="svelte"> element in src/app.html
+      target: '#svelte',
+  adapter: adapter(),
+  vite: {
+    resolve: {
+      alias: {
+        $static: path.resolve('static')
       }
     }
-	}
+  }
+  },
+
+  preprocess: [mdsvex(mdsvexConfig)]
 };
 
 export default config;
