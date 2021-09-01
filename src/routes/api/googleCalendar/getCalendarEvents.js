91facb0385e398cc06d3adafc843510395491cd5
Michael Lamb <mflambjr@gmail.com>
2021-08-31 23:18:45 -0400

    debugging

--- src/routes/api/googleCalendar/getCalendarEvents.js
+++ src/routes/api/googleCalendar/getCalendarEvents.js
@@ -43,7 +43,7 @@ export async function get() {
             body: JSON.stringify({ tag: `${entry.summary}` })
           })
 
-          entry.menu = await menu.json()
+          entry.menu = await menu.json() || {}
 
         }
 
