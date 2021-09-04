ca63195005a7c74703a3f4cd02b618ed4f69d68b
Michael Lamb <mflambjr@gmail.com>
2021-09-04 01:48:43 -0400

    footer w/ copyright & social icons

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,35 @@
+<script>
+  import Social from '$lib/components/Social.svelte'
+  import Stack from '$lib/components/layout/Stack.svelte'
+
+</script>
+
+<footer>
+  <Stack>
+    <Social />
+    <span>&#169; The Fabulous Fig - {new Date().getFullYear()}</span>
+  </Stack>
+</footer>
+
+<style>
+  footer {
+    padding: var(--s3) var(--s-3);
+    background: var(--color-dark);
+    color: var(--color-white);
+  }
+  
+  footer :global(.stack) {
+    width: fit-content;
+    margin: 0 auto;
+  }
+
+  span {
+    text-align: center;
+    /* hack to make it look more centered beneath the social icons */
+    margin-right: var(--s0);
+  }
+
+  footer :global(a) {
+    color: var(--color-light);
+  }
+</style>
