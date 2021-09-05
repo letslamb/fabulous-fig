c57e2ff855b3f63175c989a0b9f55147fbfc7e95
Michael Lamb <mflambjr@gmail.com>
2021-09-05 17:42:50 -0400

    move some page styles out of calendar comp & render calendar comp only if events exist

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,6 +30,10 @@
 
   import CalendarDisplay from '$lib/components/homePage/CalendarDisplay.svelte'
   import Hero from '$lib/components/homePage/Hero.svelte'
+  import Section from '$lib/components/utils/Section.svelte'
+  import Center from '$lib/components/layout/Center.svelte'
+  import Box from '$lib/components/layout/Box.svelte'
+
 
   import { setContext } from 'svelte'
 
@@ -43,7 +47,29 @@
 
 <Hero />
 
-<CalendarDisplay />
+<Section wrapperClass="calendar-wrapper-section">
+  <Center>
+    <Box wrapperClass={"calendar-box"}>
+      {#if calendar[0]}
+        <CalendarDisplay />
+      {:else}
+        <p>This is a placeholder for when there are no calendar events</p>
+      {/if}
+    </Box>
+  </Center>
+</Section>
+
+<style>
+  :global(.calendar-wrapper-section) {
+    max-height: fit-content;
+  }
+
+  :global(.calendar-box) {
+    --color: var(--color-dark);
+    --background-color: var(--color-light);
+    padding: var(--s-3);
+  }
+</style>
 
 
 
