ddba33a2e2534fb493d522d5ba8d507557d19ec1
Michael Lamb <mflambjr@gmail.com>
2021-09-07 02:15:21 -0400

    global seo data is returned from __layout's context object

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,12 +5,26 @@
   import Header from '$lib/components/Header.svelte'
   import Footer from '$lib/components/Footer.svelte'
   import Sprite from '$lib/components/utils/Sprite.svelte'
-  // import BaseSEO from '$lib/components/BaseSEO.svelte'
+
+  export async function load({page}) {
+
+    const { host, path } = page
+
+    const context = {
+      seo: {
+        siteName: 'The Fabulous Fig Vegan Food Truck',
+        locale: 'en_US',
+        canonical: `https://${page.host}${page.path}`
+      }
+    }
+    return {
+      context
+    }
+  }
 
 
 </script>
 
-<!-- <BaseSEO /> -->
 <svelte:head>
   <meta name="robots" content="noindex">
 </svelte:head>
