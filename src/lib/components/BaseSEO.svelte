21f6a23558c44cce8a807f7352fdf97f2afdb84d
Michael Lamb <mflambjr@gmail.com>
2021-09-07 02:14:05 -0400

    on-page seo comp gets data via getContext

---
+++ src/lib/components/BaseSEO.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { getContext } from 'svelte'
+
+  let data = getContext('seo')
+
+  const { global } = data
+  const [ page ] = data.page
+
+  $: console.log(`HERE'S WHAT'S COMING INTO BASESEO: ${JSON.stringify(data, null, 2)}`)
+  $: console.log(`GLOBAL: ${JSON.stringify(global, null, 2)}`)
+  $: console.log(`PAGE: ${JSON.stringify(page, null, 2)}`)
+</script>
+
+<svelte:head>
+  <title>{page.title}</title>
+  <link rel="canonical" href={global.canonical} />
+  <meta name="description" content={page.description} />
+  <meta property="og:description" content={page.description} />
+  <meta property="og:locale" content={global.locale} />
+  <meta property="og:site_name" content={global.siteName} />
+  <meta property="og:title" content={page.title} />
+  <meta property="og:image" content={page.images.facebook.url} />
+  <meta property="og:image:width" content={page.images.facebook.width} />
+  <meta property="og:image:height" content={page.images.facebook.height} />
+  <meta property="og:image:alt" content={page.altText} />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content={global.canonical} />
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:creator" content="@letslamb" />
+  <meta name="twitter:title" content={global.siteName} />
+  <meta name="twitter:description" content={page.description} />
+  <meta name="twitter:image" content={page.images.twitter.url} />
+  <meta name="twitter:image:alt" content={page.altText} />
+</svelte:head>
