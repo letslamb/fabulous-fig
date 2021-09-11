ec39b7cb0aed2410b0220ff2186c4d615a05e23b
Michael Lamb <mflambjr@gmail.com>
2021-09-11 18:42:22 -0400

    add Center to keep logo svg centered explicitly (not incidentally)

--- src/lib/components/homePage/Hero.svelte
+++ src/lib/components/homePage/Hero.svelte
@@ -1,10 +1,17 @@
+<script>
+  import Center from '$lib/components/layout/Center.svelte'
+</script>
+
 
 <svelte:options immutable={true} />
 
 <div>
-  <svg viewBox="0 0 900 1164" xlmns="http://www.w3.org/2000/svg" role="img" aria-label="The Fabulous Fig Logo">
-    <use href={`#fabulous-fig-logo`} width="100%" height="100%"></use>
-  </svg>
+  <Center>
+    <svg viewBox="0 0 900 1164" xlmns="http://www.w3.org/2000/svg" role="img" aria-label="The Fabulous Fig Logo">
+      <use href={`#fabulous-fig-logo`} width="100%" height="100%"></use>
+    </svg>
+  </Center>
+
 </div>
 
 <style>
