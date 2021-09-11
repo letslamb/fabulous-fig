810dfb0a34917448bb846fde1f2ce8fc5168aae4
Michael Lamb <mflambjr@gmail.com>
2021-09-11 18:44:36 -0400

    change <Frame> so it doesn't require <img> to be direct descendant

--- src/lib/components/layout/Frame.svelte
+++ src/lib/components/layout/Frame.svelte
@@ -41,10 +41,38 @@
   align-items: center;
 }
 
-:global(.frame > img),
+/* TODO - keep an eye on this, the original comp. had img as a direct child of <Frame>
+but we canged it so that <Loader> could slot into <Frame> instead of wrapping it,
+thus preserving the browser's pre-calculated aspect-ratio. Watch to see if
+you have any problems with this component elsewhere */
+:global(.frame img),
 :global(.frame > video) {
   width: 100%;
   height: 100%;
   object-fit: cover;
 }
+
+/* .frame {
+  padding-bottom: calc(var(--numerator, 1) / var(--denominator, 1) * 100%);
+  position: relative;
+}
+
+:global(.frame > *) {
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+:global(.frame > img),
+:global(.frame > video) {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+} */
 </style>
