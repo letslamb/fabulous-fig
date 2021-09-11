9b157b58d9042b572bc6663ece788aedf5825172
Michael Lamb <mflambjr@gmail.com>
2021-09-10 23:48:47 -0400

    remove commented out code, add immutable={true} svelte:option

--- src/lib/components/homePage/Hero.svelte
+++ src/lib/components/homePage/Hero.svelte
@@ -1,18 +1,5 @@
-<script>
-  // import Sprite from '/svg/icons.svg'
-  // import { getContext } from 'svelte'
 
-  // let sprite
-
-  // if (getContext('svgSprite')) {
-  //   console.log(`svg sprite in getContext: ${getContext('svgSprite')}`)
-  //   sprite = getContext('svgSprite')
-  // } else {
-  //   console.log(`we didn't find the sprite, trying async import now`)
-  //   sprite = async() => await import('/svg/icons.svg')
-  // }
-
-</script>
+<svelte:options immutable={true} />
 
 <div>
   <svg viewBox="0 0 900 1164" xlmns="http://www.w3.org/2000/svg" role="img" aria-label="The Fabulous Fig Logo">
