b076db2afefc75c141b38b8d183236f67b7c3c54
Michael Lamb <mflambjr@gmail.com>
2022-02-24 22:54:20 -0500

    remove outer html & styles

--- src/lib/components/homePage/Hero.svelte
+++ src/lib/components/homePage/Hero.svelte
@@ -1,22 +1,7 @@
-<script>
-  import Center from '$lib/components/layout/Center.svelte'
-</script>
-
 
 <svelte:options immutable={true} />
 
-<div>
-  <Center>
-    <svg viewBox="0 0 900 1164" xlmns="http://www.w3.org/2000/svg" role="img" aria-label="The Fabulous Fig Logo">
-      <use href={`#fabulous-fig-logo`} width="100%" height="100%"></use>
-    </svg>
-  </Center>
+<svg viewBox="0 0 900 1164" xlmns="http://www.w3.org/2000/svg" role="img" aria-label="The Fabulous Fig Logo">
+  <use href={`#fabulous-fig-logo`} width="100%" height="100%"></use>
+</svg>
 
-</div>
-
-<style>
-  svg {
-    display: block;
-    margin: 0 auto;
-  }
-</style>
