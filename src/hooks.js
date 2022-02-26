6f08f18b561f5150c80f2f9016784058aadf6bac
Michael Lamb <mflambjr@gmail.com>
2022-02-26 01:59:05 -0500

    configure prismic link resolver

--- src/hooks.js
+++ src/hooks.js
@@ -1,3 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import PrismicDOM from 'prismic-dom';
+const { PRISMIC_API_ENDPOINT } = process.env;
+import { linkResolver } from '$lib/js/utils'
+
+export async function handle({ event, resolve }) {
+	event.locals.ctx = {
+		endpoint: PRISMIC_API_ENDPOINT,
+    linkResolver: linkResolver
+  };
+
+	event.locals.DOM = PrismicDOM;
+
+	const response = await resolve(event);
+
+  return response
+
+}
+
 export async function handleError({ error, event }) {
   console.error(error, { event })
 }
