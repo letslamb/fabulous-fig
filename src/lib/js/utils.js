89bceabb85a1a3823b4aede5fd286b6f84af355f
Michael Lamb <mflambjr@gmail.com>
2022-02-26 02:18:28 -0500

    delete commented out code

--- src/lib/js/utils.js
+++ src/lib/js/utils.js
@@ -5,3 +5,19 @@ export function capitalizeEachLetter(phrase) {
     return word[0].toUpperCase() + word.substring(1)
   }).join(" ")
 }
+
+export function linkResolver(doc) {
+	if (doc.type === 'menu_layout') {
+    return '/menu/' + doc.uid + '/'
+  }
+
+	return '/';
+}
+
+// navLinks = linkSlice.items.map(item => {
+//   if (item.nav_link.type === "homepage") {
+//     return { href: '/', text: 'Home'}
+//   } else if (item.nav_link.type === "menu_layout") {
+//     return { href: `/menu/${item.nav_link.uid}`, text: `${capitalizeEachLetter(item.nav_link.uid)} Menu`}
+//   }
+// })
